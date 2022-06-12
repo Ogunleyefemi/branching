053ae873ea372a3bd138552e0e30f4bea6711f60
oluwafemi <oluwafemiogunleye21@gmail.com>
2022-06-12 13:18:43 +0100

    add function of addition

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let firstvalue = parseInt(prompt("input first value"));
+let secondvalue = parseInt(prompt("input first value"));
+let thirdvalue = parseInt(prompt("input first value"));
+let sum = (firstvalue + secondvalue + thirdvalue);
+// to check if sum is even or odd
+function addition(){
+    return sum;
+}
+// given conditions
+if (sum % 2 === 0){
+    alert("sum of your value is even");
+}
+else{
+    alert("sum of your value is odd");
+}
+
+
+
